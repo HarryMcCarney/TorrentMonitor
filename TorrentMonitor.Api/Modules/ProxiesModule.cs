75943ec1a6d5d5210d67cad726e2677525fcc060
Harry McCarney <hmccarney@gmail.com>
2014-06-03 22:17:56 +0200

    added redis caching of proxy list

--- TorrentMonitor.Api/Modules/ProxiesModule.cs
+++ TorrentMonitor.Api/Modules/ProxiesModule.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using HtmlAgilityPack;
-using Nancy;
+﻿using Nancy;
 
 namespace TorrentMonitor.Api.Modules
 {
@@ -15,35 +11,7 @@ namespace TorrentMonitor.Api.Modules
 
         private Response GetProxies()
         {
-            var proxies = new List<PBProxy>();
-
-            using (var client = new WebClient())
-            {
-
-                var html = new HtmlWeb().Load("http://proxybay.info/");
-                var nodes = html.DocumentNode.SelectNodes("//tr");
-                foreach (var a in nodes)
-                {
-
-                    var site = a.SelectNodes("td[contains(@class,'site')]");
-
-                    if (site != null)
-                    {
-                        var proxy = new PBProxy();
-                        var url = site.FindFirst("a").Attributes["href"].Value;
-                        var status =
-                            ResolveStatus(
-                                a.SelectNodes("td[contains(@class,'status')]").FindFirst("img").Attributes["src"].Value);
-                        var speed = a.SelectNodes("td[contains(@class,'speed')]").Nodes().First().InnerText;
-
-                        proxy.Url = url;
-                        proxy.Status = status;
-                        proxy.Speed = speed;
-
-                        proxies.Add(proxy);
-                    }
-                }
-            }
+            var proxies = new ProxyListFetcher().Fetch();
 
             return Response.AsJson(proxies);
         }
